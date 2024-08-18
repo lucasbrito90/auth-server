7cbeb613cb35ca80f7732dd70dd8124a1d87a635
Lucas Brito de Almeida <lucas_brito@outlook.com>
2024-08-17 23:10:29 -0400

    init

---
+++ app/Models/Client.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Laravel\Passport\Client as PassportClient;
+
+class Client extends PassportClient
+{
+    public function skipsAuthorization(): bool
+    {
+        return true;
+    }
+}
